52f6da8218688bee8cffccc4cda9675b568ce4ac
Jeimissonb <jeimissonb@hotmail.com>
2022-01-14 11:40:24 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/controller/MovieController.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsmovie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.service.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+	@Autowired
+	private MovieService service;
+	
+	@GetMapping
+	public Page<MovieDTO> findAll (Pageable pageable){
+		Page <MovieDTO> page = service.findAll(pageable);
+		return page;
+	}
+	
+	@GetMapping("/{id}")
+	public MovieDTO findById (@PathVariable Long id) {
+		MovieDTO movie = service.findById(id);
+		return movie;
+	}
+}
